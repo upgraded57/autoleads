8f02c01d02ca70337cca8ab7c697ddcccd60b143
upgraded57 <shokunbiupgraded@gmail.com>
2024-09-19 13:59:15 +0100

    Minor bug fix check

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,30 @@
-import "./header.css";
 import logo from "../../assets/logo-alt.png";
 import { Link, useNavigate } from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
   const navigate = useNavigate();
+  const navLinks = [
+    {
+      path: "#",
+      title: "Resources",
+    },
+    {
+      path: "/features",
+      title: "Features",
+    },
+    {
+      path: "/pricing",
+      title: "Pricing",
+    },
+    {
+      path: "/about",
+      title: "About Us",
+    },
+  ];
   const [navOpen, setNavOpen] = useState(false);
 
   const toggleNav = () => setNavOpen((prev) => !prev);
@@ -20,39 +38,35 @@ export default function Header() {
     }
   };
   return (
-    <header>
-      <div className="logo">
+    <header className="flex items-center justify-between py-4 h-[100px]">
+      <div className="basis-1/4">
         <Link to="/">
-          <img src={logo} alt="Primeclick logo" />
+          <img src={logo} alt="Primeclick logo" className="w-[150px]" />
         </Link>
       </div>
-      <nav className={navOpen ? "active" : ""}>
-        <div className="nav-close-btn" onClick={toggleNav}>
-          <IoIosCloseCircleOutline />
-        </div>
-        <div className="nav-logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <ul>
-          <li>
-            <Link to="#">Resources</Link>
-          </li>
-          <li>
-            <Link to="/features">Features</Link>
-          </li>
-          <li>
-            <Link to="/pricing">Pricing</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
+
+      <nav className="basis-2/4">
+        <ul className="flex items-center justify-between w-full">
+          {navLinks.map((el, idx) => (
+            <li key={idx}>
+              <Link to={el.path}>{el.title}</Link>
+            </li>
+          ))}
         </ul>
-        <div className="auth-links">
-          <button onClick={login}>Log in</button>
-          <button onClick={() => navigate("/auth?type=signup")}>Sign up</button>
-        </div>
       </nav>
-      <div className="burger" onClick={toggleNav}>
+      <div className="flex items-center justify-end gap-4 basis-1/4">
+        <Button variant="ghost" onClick={login} className="px-6">
+          Log in
+        </Button>
+        <Button
+          className="pry-btn px-6"
+          onClick={() => navigate("/auth?type=signup")}
+        >
+          Sign up
+        </Button>
+      </div>
+
+      <div className="text-2xl cursor-pointer md:hidden" onClick={toggleNav}>
         <HiMiniBars3 />
       </div>
     </header>
